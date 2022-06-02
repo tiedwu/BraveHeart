2e915cb28d8e88ace04adb05e9f7cc146e5b95de
tiedwu <tiedwu@gmail.com>
2022-06-02 17:18:29 +0800

    upload pngs

--- root-hpbar.py
+++ root-hpbar.py
@@ -1,12 +1,17 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+from kivy.core.window import Window
 
+Window.size = 1440, 800
+
+
+WIDTH = 1440
 
 Builder.load_string('''
 <Main>:
-	width: root.width
-	height: root.height
+	width: root.ww
+	height: root.wh
 	canvas:
 		Color:
 			rgba: 1, 1, 1, 1
@@ -20,6 +25,7 @@ Builder.load_string('''
 		size_hint: None, None
 		pos: [0, 0.5 * root.height]
 		size: [0.5 * root.width, 0.5 * root.height]
+		window_width: 1440
 
 		# hp full: 100%
 		hp_remain: 100
@@ -28,26 +34,42 @@ Builder.load_string('''
 				rgba: 0, 0, 0, 1
 			Rectangle:
 				pos: 0, 0
-				size: self.size
+				size: top_left.size
 
 			# draw hp bar
 			Color:
 				rgba: 1, 1, 1, 1
 			Rectangle:
 				#id: hpbar
-				pos: self.size[0] * 0.2, self.size[1] * 0.8
-				size: self.size[0] * 0.5, 5
+				pos: top_left.size[0] * 0.1, top_left.size[1] * 0.8
+				#size: self.size[0] * 0.5, 30
+				size: top_left.size[0] * 0.4, 30
 			Color:
 				rgba: 0, 1, 0, 1
 			Rectangle:
-				pos: self.size[0] * 0.2, self.size[1] * 0.8
-				size: self.size[0] * 0.5 * (self.hp_remain / 100), 5
+				pos: self.size[0] * 0.1, self.size[1] * 0.8
+				size: self.size[0] * 0.5 * (self.hp_remain / 100), 30
+
+		Image:
+			size_hint: None, None
+			source: 'src/icons/hp.png'
+			pos_hint: {'x': 0, 'y': 0.7}
+			size: [top_left.window_width/(top_left.window_width/80),\
+					top_left.window_width/(top_left.window_width/80)]
 
 		Label:
 			id: show_hp
 			size_hint: None, None
-			pos_hint: {'x': 0.1, 'y': 0.6}
-			text: "HP: 100%"
+			#pos_hint: {'x': 0.8, 'y': 0.6}
+			size: top_left.size[0] * 0.2, self.size[1]
+			pos_hint: {'x': 0.4, 'y': 0.63}
+			#x: top_left.size[0] * 0.3
+			#pos_hint_y: 0.6
+			#y: top_left.size[1] * 0.6
+			text_size: self.size
+			text: "1234/1234"
+			halign: 'right'
+			valign: 'middle'
 
 		Button:
 			size_hint: None, None
@@ -126,6 +148,17 @@ Builder.load_string('''
 ''')
 
 class Main(Widget):
+
+	ww, wh = Window.size
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+
+		# root width, height
+		print("main: ", self.ww, self.wh)
+
+		# top_left.size
+		print("top_left size: ", self.ids.top_left.size)
+
 	def press_me(self):
 		remain = self.ids.top_left.hp_remain
 		remain -= 25
