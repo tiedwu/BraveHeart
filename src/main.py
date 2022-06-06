42273427f3e8a59b6cc2dcdc9a25c87369b9a770
tiedwu <tiedwu@gmail.com>
2022-06-06 20:27:33 +0800

    profile layouts

--- src/main.py
+++ src/main.py
@@ -85,6 +85,182 @@ Builder.load_string('''
 		valign: 'middle'
 		text: "227636.06" + " "
 
+<PFLabel@Label>:
+	font_size: '14sp'
+	font_name: 'fonts/DroidSansFallback.ttf'
+
+<PFBox>:
+	padding: 20
+	canvas.before:
+		Color:
+			rgba: utils.get_color_from_hex('#33322F7F')
+		Rectangle:
+			size: self.size
+			pos: self.pos
+
+	RelativeLayout:
+		width: root.ww * 0.5 - 40
+		height: root.wh * 0.18 - 40
+		id: top_left
+		size_hint: None, None
+		#pos: [0, 0.5 * root.height]
+		#size: [0.5 * root.width, 0.5 * root.height]
+		window_width: 1440
+
+		# hp full: 100%
+		hp_remain: 100
+		canvas:
+			#Color:
+			#	rgba: 0, 0, 0, 1
+			#Rectangle:
+			#	pos: 0, 0
+			#	size: self.size
+
+			# draw hp bar
+			Color:
+				rgba: 1, 1, 1, 1
+			Rectangle:
+				pos: top_left.size[0] * 0.15, top_left.size[1] * 0.9
+				size: top_left.size[0] * 0.85, 30
+			Color:
+				rgba: 0, 1, 0, 1
+			Rectangle:
+				pos: self.size[0] * 0.15, self.size[1] * 0.9
+				#size: self.size[0] * 0.7 * (self.hp_remain / 100), 30
+				size: self.size[0] * 0.85, 30
+
+		# HP
+		Image:
+			size_hint: None, None
+			source: 'icons/hp.png'
+			pos_hint: {'x': 0, 'y': 0.82}
+			size: [top_left.window_width/(top_left.window_width/80),\
+					top_left.window_width/(top_left.window_width/80)]
+
+		Label:
+			#font_size: '10sp'
+			id: show_hp
+			size_hint: None, None
+			size: top_left.size[0] * 0.6, self.size[1]
+			pos_hint: {'x': 0.4, 'y': 0.73}
+			text_size: self.size
+			text: "45979/45979"
+			halign: 'right'
+			valign: 'middle'
+
+		# ATTACK
+		Image:
+			size_hint: None, None
+			source: 'icons/attack.png'
+			pos_hint: {'x': 0, 'y': 0.55}
+			#size: [top_left.window_width/(top_left.window_width/80),\
+			#	top_left.window_width/(top_left.window_width/80)]
+
+		PFLabel:
+			id: show_attack
+			size_hint: None, None
+			#font_size: '12sp'
+			size: top_left.size[0] * 0.35, self.size[1]
+			x: top_left.size[0] * 0.1
+			y: top_left.size[1] * 0.55
+			text_size: self.size
+			text: "45808"
+			halign: 'right'
+			valign: 'middle'
+
+		# critical rate
+		Image:
+			size_hint: None, None
+			source: 'icons/crit_rate.png'
+			pos_hint: {'x': 0.5, 'y': 0.55}
+			#size: [top_left.window_width/(top_left.window_width/80),\
+			#	top_left.window_width/(top_left.window_width/80)]
+
+		PFLabel:
+			id: show_critrate
+			size_hint: None, None
+			size: top_left.size[0] * 0.6, self.size[1]
+			x: top_left.size[0] * 0.4
+			y: top_left.size[1] * 0.55
+			#font_size: '12sp'
+			text_size: self.size
+			text: "102%"
+			halign: 'right'
+			valign: 'middle'
+
+		# crit damage
+		Image:
+			size_hint: None, None
+			source: 'icons/crit_dmg.png'
+			pos_hint: {'x': 0, 'y': 0.3}
+
+		PFLabel:
+			id: show_critdmg
+			size_hint: None, None
+			#font_size: '12sp'
+			size: top_left.size[0] * 0.35, self.size[1]
+			x: top_left.size[0] * 0.1
+			y: top_left.size[1] * 0.3
+			text_size: self.size
+			text: "1124%"
+			halign: 'right'
+			valign: 'middle'
+
+		# armor
+		Image:
+			size_hint: None, None
+			source: 'icons/armor.png'
+			pos_hint: {'x': 0.5, 'y': 0.3}
+
+		PFLabel:
+			id: show_armor
+			size_hint: None, None
+			size: top_left.size[0] * 0.6, self.size[1]
+			x: top_left.size[0] * 0.4
+			y: top_left.size[1] * 0.3
+			font_size: '11sp'
+			text_size: self.size
+			text: "4648\\n(94.85%) "
+			halign: 'right'
+			valign: 'middle'
+
+		# block
+		Image:
+			size_hint: None, None
+			source: 'icons/block.png'
+			pos_hint: {'x': 0, 'y': 0.05}
+			#size: 125, 125
+
+		PFLabel:
+			id: show_block
+			size_hint: None, None
+			#font_size: '12sp'
+			size: top_left.size[0] * 0.35, self.size[1]
+			x: top_left.size[0] * 0.1
+			y: top_left.size[1] * 0.05
+			text_size: self.size
+			text: "4128"
+			halign: 'right'
+			valign: 'middle'
+
+		# profile
+		Image:
+			size_hint: None, None
+			source: 'icons/profile.png'
+			pos_hint: {'x': 0.5, 'y': 0.05}
+
+		PFLabel:
+			id: show_profile
+			size_hint: None, None
+			size: top_left.size[0] * 0.6, self.size[1]
+			x: top_left.size[0] * 0.4
+			y: top_left.size[1] * 0.05
+			#font_size: '11sp'
+			text_size: self.size
+			text: "详细属性"
+			halign: 'right'
+			valign: 'middle'
+
 <RootWidget>:
 	canvas:
 		Color:
@@ -123,8 +299,16 @@ Builder.load_string('''
 		y: 0.95 * root.wh
 		size_hint: 0.24, 0.03
 
+	PFBox:
+		x: 0
+		y: 0.75 * root.wh
+		size_hint: 0.5, 0.18 
+
 
 ''')
+
+class PFBox(BoxLayout):
+	ww, wh = Window.size
 
 class CEBox(BoxLayout):
 	pass
