1a4b8149f4e8e69bf9fae713780e6a929331f268
tiedwu <tiedwu@gmail.com>
2022-06-08 19:07:30 +0800

    eq db update

--- utils/eqs.py
+++ utils/eqs.py
@@ -6,23 +6,57 @@
 # Ex: 独特 B, QAC = 45
 # 暴伤(damage): D(70%-75%), C(75%-80%), B(80%-85%), A(85%-90%), S(90%-95%), SS(95%-100%), SSS(100%-105%), 不朽I(105%-110%), 不朽II(110%-115%)
 # 暴击(crit): D(11%), C(12%), B(13%), A(14%), S(15%-17%), SS(18%-20%), SSS(21%-23%), 不朽I(24%-26%), 不朽II(27%-29%)
+
+# 新手剑: attack = 50 * QAC + 0.9 * LV
+# 普通长剑: attack = 62 * QAC + 0.9 * LV
+# 战士长剑: attack = 74 * QAC + 1 * LV, armor = 7 * QAC + 0.9 * LV
+# 毛毛的爪子: attack = 76 * QAC + 1.1 * LV, crit (ref to quality)
+# 冰晶之刃: attack = 88 * QAC + 1.2 * LV, damage (ref to quality)
+# 赤柳血刃: attack = 90 * QAC + 1.2 * LV, hp = 100 * QAC + 0.9 * LV
+# 狱岩石太刀: attack = 92 * QAC + 1.2 * LV, crit (ref to quality)
+# 紫炎波刃剑: attack = 94 * QAC + 1.4 * LV
+
+# 大师大冒险家之剑: attack = 100 * QAC + 1.8 * LV, hp = 150 * QAC + 1.8 * LV
+# 数珠丸恒次: attack = 100 * QAC + 4 * LV
+# 埃苏莱布斯军刃: attack = 100 * QAC + 2.1 * LV, armor = 20 * QAC + 1.6 * LV, block = 10 * QAC + 1 * LV
+# 无名剑: attack = 100 * QAC + 2.7 * LV, crit (ref to quality) * 2.5
+# 死亡之刃: attack = 100 * QAC + 2 * LV, crit (ref to qulity) * 1.5, damage (ref to quality) * 1.3
+# 霜龙利刃: attack = 100 * QAC + 1.9 * LV, crit (ref to qulity) * 1.6, damage (ref to quality) * 1.3
+# 阿加雷斯血色巨剑: attack = 100 * QAC + 1.8 * LV, crit (ref to qulity) * 1.5, damage (ref to quality) * 1.4
+# 神龙纳格林之刃: attack = 100 * QAC + 2.8 * LV, damage (ref to quality) * 2
+# 六翼天使武刃: attack = 100 * QAC + 2.6 * LV, damage (ref to quality) * 1.2, armor = 20 * QAC + 1.1 * LV
+# 黑色冰虎刃: attack = 100 * QAC + 2 * LV, armor = 20 * QAC + 1.2 * LV, hp = 150 * QAC + 1.1 * LV
+# 繁星之剑: attack = 100 * QAC + 1.7 * LV, damage (ref to quality) * 1, hp = 150 * QAC + 1 * LV
+# 心碎瞬间短剑: attack = 100 * QAC * QAC + 2 * LV, damage (ref to qulity) * 1.4, armor = 20 * QAC + 0.9 * LV
+
+# 新手铠甲: armor = 10 * QAC + 0.7 * LV
+# 战士重铠: armor = 13 * QAC + 0.8 * LV, hp = 140 * QAC + 1.1 * LV
+# 天权軽甲: armoa = 11 * QAC + 0.7 * LV, hp = 130 * QAC + 1.2 * LV, attack = 5 * QAC + 1 * LV
+# 赤柳血铠: armor = 13 * QAC + 0.8 * LV, hp = 140 * QAC + 1.3 * LV
+# 紫金守护胸甲 armor = 14 * QAC + 1 * LV, hp = 150 * QAC + 1.4 * LV
+# 哈皮毛毛连身衣: armor = 13 * QAC + 0.9 * LV, hp = 130 * 19 + 1.2 * 198 = 2840, attack =
+
+# 剑豪盔甲: armor = 15 * QAC + 2.1 * LV, hp = 200 * QAC + 2.6 * LV
+# 肃清者戎衣: hp = 200 * QAC + 1.8 * LV, attack = 16 * QAC + 2.5 * LV, block = 10 * QAC + 1.3 * LV
+# 红月的夜行衣: armor = 15 * QAC + 1.2 * LV, hp = 200 * QAC + 1.7 * LV, attack = 16 * QAC + 1.2 * LV
+# 隐武士铠甲: armor = 15 * QAC + 1.3 * LV, hp = 200 * QAC + 1.9 * LV, attack = 16 * QAC + 0.9 * LV
+# 芬萨里尔追踪者: armor = 15 * QAC + 1.1 * LV, damage (ref to quality) * 1.6, attack = 16 * QAC + 1.8 * LV
+# 黑镇月: armor = 15 * QAC + 2.6 * LV, hp = 200 * QAC + 2.1 * LV
+# 破坏者H-018: armor = 15 * QAC + 1.6 * LV, hp = 200 * QAC + 1.6 * LV, damage (ref to quality) * 1.6
+# 心化乱坠长袍: armor = 15 * QAC + 1.8 * LV, hp = 200 * QAC + 1.4 * LV, attack = 16 * QAC + 1.5 * LV
+# 法夜: armor = 15 * QAC + 1.2 * LV, hp = 200 * QAC + 0.6 * LV, damage (ref to quality) * 2.1
+
+# 新手项链: damage (ref to quality)
+# 冰龙凝雪: damage (ref to quality), crit (ref to quality), hp = 90 * QAC + 0.5 * LV
+# 银魄之眼: crit (ref to quality), hp = 100 * QAC + 0.5 * LV, attack = 70 * QAC + 0.6 * LV
+# 十字军项链: armor = 0.9 * QAC + 1.2 * LV, hp = 100 * QAC + 0.9 * LV, block = 0.9 * QAC + 1 * LV
+
+# 伟大单身成员的项链: damage (refer to quality) * 1.1, block = 1 * QAC + 1.3 * LV, hp = 110 * QAC + 1 * LV
+# 十字旅团降魔项链: attack = 12 * QAC + 0.7 * LV, hp = 110 * QAC + 0.8 * LV, armor = 0.9
+
+# 新手指环: hp = 20 * QAC + 1 * LV
+# 毛毛指环: hp = 10 * QAC + 15.1 * LV, attack = 5 * QAC + 0.2 * LV, crit (ref to quality)
 # 生命指环: hp = 20 * QAC + 15.2 * LV
-# 十字军项链: armor = 5 * QAC + 1.16 * LV, hp = 19 * QAC + 4.86 * LV, block = 3 * 5 + ?
-# 天权軽甲: armoa = 3 * QAC + 0.81 * LV, hp = 10 * QAC + 8.33 * LV, attack = 1 * QAC + 0.93 * LV
-# 毛毛的爪子: attack = 28 * QAC + 1.72 * LV, crit (ref to quality)
-# 战士重铠: armor = 11 * QAC + 1.59 * LV, hp = 55 * QAC + 10.47 * LV
-# 冰晶之刃: attack = 10 * QAC + V * LV, damage (ref to quality)
-# 银魄之眼: crit (ref to quality), hp = 20 * QAC + 4.94 * LV, attack = 10 * QAC + 0.74 * LV
-# 战士长剑: attack = 10 * QAC + 1.84 * LV, armor = 3 * QAC + 1.56 * LV
-# 冰龙凝雪: damage (ref to quality), crit (ref to quality), hp = 28 * QAC + 5.43 * LV
-# 毛毛指环: hp = 10 * QAC + 15.04 * LV, attack = 5 * QAC + 0.13 * LV, crit (ref to quality)
-# 赤柳血刃: attack = 6 * QAC + 1.56 * LV, hp = 39 * QAC + 13.09 * LV
-# 赤柳血铠: armor = 10 * QAC + 1.33 * LV, hp = 20 * QAC + 19.47 * LV
-# 普通长剑: attack = 20 * QAC + 2.02 * LV
-# 狱岩石太刀: attack = 15 * QAC + 0.73 * LV, crit (ref to quality)
-# 紫炎波刃剑: attack = 17 * QAC + 1.8 * LV
-# 大师大冒险家之剑: attack = 20 * QAC + 1.78 * LV, hp = 20 * QAC + 39.22 * LV
-# 数珠丸恒次: attack = 20 * QAC + 1.29 * LV
 
 weapons = [
     ['新手剑', '一把普通的剑', '破旧', 'attack=10+7(5), (crit=10), lv=1'],
@@ -34,7 +68,8 @@ weapons = [
     ['狱岩石太刀', '用狱岩石制作的太刀, 据说拥有让使用者潜力爆发的神秘力量', '史诗', 'C: attack=578, B: crit=22, (hp=1335, armor=369, damage=41, crit=15), lv=197'],
     ['紫炎波刃剑', '传说中狂战士最喜爱的剑', '史诗', 'B: attack=886, (damage=46, armor-addi=23.4, hp=1390, crit=13), lv=200'],
     ['大师大冒险家之剑', '大师大冒险家之剑', '独特', 'B: attack=1248, C: hp=8546, (hp=1605, attack=496, damage=36, attack=537, armor-addi=42.95), lv=196'],
-    ['数珠丸恒次', '具体情况不明， 传说为日莲上人所有', 'D: attack=943+1197(13), (attack=507, crit=21, armor=313, hp=1198, attack-addi=32.33), lv=96'],
+    ['数珠丸恒次', '具体情况不明， 传说为日莲上人所有', '独特', 'D: attack=943+1197(13), (attack=507, crit=21, armor=313, hp=1198, attack-addi=32.33), lv=96'],
+    ['埃苏莱布斯军刃', '', '独特', 'A: attack=1346, C: armor=727, A: block=364, (hp-addi=24.08, armor=276, armor-addi=38.73, attack=655, attack-addi=38.27), lv=197'],
 ]
 
 suits = [
@@ -42,6 +77,7 @@ suits = [
     ['战士重铠', '六级战士使用的重型铠甲', '神器', 'C: armor=477, D: hp=2776, (hp=1054, armor-addi=18.48, armor-addi=26.87), lv=197'],
     ['天权轻甲', '舍弃防御性能的轻甲，因为更加轻便, 攻击性能更加突出', '神器', 'B: armor=3， A: hp=10, A: attack=1, (hp-addi=7.1, hp=6, attack=3), lv=1'],
     ['赤柳血铠', '似乎会给使用者提供生命气息', '神器', 'B: armor=430, C: hp=4115, (hp=1082, armor-addi=22.11, armor=171), lv=196'],
+    ['紫金守护胸甲', '够肉才能输出', '神器', 'D: armor=771, D:hp=2128, (hp=1116, armor-addi=29.18, armor-addi=21.98), lv=199'],
 ]
 
 necks = [
